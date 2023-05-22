540ad484baab976c7412c60d795bb2d81e79ad1c
Sidgap <sidhantsharma712@gmail.com>
2023-05-22 13:26:06 +0530

    Initial setup

---
+++ src/test/java/com/learn2code/vehicleapi/serach/VehicleSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn2code.vehicleapi.serach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
